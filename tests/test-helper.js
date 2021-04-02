9a9a1fce3e4f66be0d489e1b38d4317a3953e006
Wout Schellaert <wout.schellaert@gmail.com>
2021-04-02 17:48:40 +0200

    Ember init

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'frontend-poc-participatie/app';
+import config from 'frontend-poc-participatie/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
