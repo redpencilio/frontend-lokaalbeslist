70b602628559db48edc080f495f4dcdd498412ee
Wout Schellaert <wout.schellaert@gmail.com>
2021-04-09 12:09:54 +0200

    Fix white space breaking of search result controls

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -3,4 +3,7 @@
 module.exports = {
   plugins: ['ember-template-lint-plugin-prettier'],
   extends: ['octane', 'ember-template-lint-plugin-prettier:recommended'],
+  rules: {
+    'no-inline-styles': false,
+  },
 };
