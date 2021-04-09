be7fb4a54664e13feff77ac900a03be23bb6a2b0
Wout Schellaert <wout.schellaert@gmail.com>
2021-04-09 11:02:03 +0200

    Better linting setup and fix lint warnings

--- tests/integration/modifiers/render-feather-icons-test.js
+++ tests/integration/modifiers/render-feather-icons-test.js
@@ -3,11 +3,11 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Modifier | render-feather-icons', function(hooks) {
+module('Integration | Modifier | render-feather-icons', function (hooks) {
   setupRenderingTest(hooks);
 
   // Replace this with your real tests.
-  test('it renders', async function(assert) {
+  test('it renders', async function (assert) {
     await render(hbs`<div {{render-feather-icons}}></div>`);
 
     assert.ok(true);
--- tests/unit/utils/mu-search-test.js
+++ tests/unit/utils/mu-search-test.js
@@ -1,10 +1,9 @@
 import muSearch from 'frontend-poc-participatie/utils/mu-search';
 import { module, test } from 'qunit';
 
-module('Unit | Utility | mu-search', function() {
-
+module('Unit | Utility | mu-search', function () {
   // TODO: Replace this with your real tests.
-  test('it works', function(assert) {
+  test('it works', function (assert) {
     let result = muSearch();
     assert.ok(result);
   });
