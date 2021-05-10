1066c169a630dd9b10c849f52844487b94f0a6c4
Wout Schellaert <wout.schellaert@gmail.com>
2021-05-10 11:26:37 +0200

    Language consistency: file names
    For language consistency all internal names (code, pathnames, ...) are
    being made English. In this commit we change all filenames only, so Git
    has an easy time knowing it's only a rename.
    
    In a following commit the paths in the code will be updated to reflect
    this.

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default class IndexRoute extends Route {
   beforeModel(/* transition */) {
-    this.transitionTo('zoek'); // Implicitly aborts the on-going transition.
+    this.transitionTo('search'); // Implicitly aborts the on-going transition.
   }
 }
