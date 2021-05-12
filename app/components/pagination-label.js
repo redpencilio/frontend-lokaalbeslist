b0ebea111c8148019874011dcfbeb5468a0055e0
Wout Schellaert <wout.schellaert@gmail.com>
2021-05-12 17:36:02 +0200

    Fix pagination labelling

--- app/components/pagination-label.js
+++ app/components/pagination-label.js
@@ -3,14 +3,21 @@ import Component from '@glimmer/component';
 export default class PaginationLabelComponent extends Component {
   get from() {
     let p = this.args.pagination;
-    return Number(p.self.number) * Number(p.first.size);
+    let index = Number(p.self.number) * Number(p.first.size);
+
+    return this.args.count ? index + 1 : index;
   }
 
   get to() {
-    let p = this.args.pagination;
-    if (p.last.number == p.self.number) {
-      return this.from + Number(p.last.size);
+    if (this.args.count === 0) {
+      return 0;
     }
-    return this.from + Number(p.self.size);
+
+    let p = this.args.pagination;
+    let from = this.from - 1;
+    if (p.last.number == p.self.number) {
+      return from + Number(p.last.size);
+    }
+    return from + Number(p.self.size);
   }
 }
